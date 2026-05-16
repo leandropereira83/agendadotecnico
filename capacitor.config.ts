9f1bf79d60ea1c72185d8adb6600c094c1dccdf9
Leandro Pereira <leandropereira83@outlook.com>
2026-05-16 12:34:03 -0300

    instalacao capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.agendadotecnico.app',
+  appName: 'agenda-do-tecnico',
+  webDir: 'dist'
+};
+
+export default config;
