d58646eb2ab96aeeb03fcab9a033757cdffb2650
Leandro Pereira <leandropereira83@outlook.com>
2026-05-16 12:54:50 -0300

    criacao rota login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { loginGuard } from './core/guards/login-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login',canActivate: [loginGuard], loadComponent: () => import('./pages/login/login').then(m => m.Login)},
+];
